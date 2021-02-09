2583c93db342bb1488c51b2d1da1fa4068998789
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 04:23:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  dolphin = 3333333
+  dolphin = 333333
   whale = nil
   name_hash.each do |key, value|
     if value < dolphin
